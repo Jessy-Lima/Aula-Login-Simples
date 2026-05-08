1ec12fb5261e7e31dd706159f548438b72e58e80
Jessica Lima <maytajessica73@gmail.com>
2026-05-08 14:01:21 -0300

    verificação de email se existe

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from database import get_db, Usuario
 
 app = FastAPI(title="Sistema de login simples")
 
@@ -36,3 +37,27 @@ def tela_inicial(request: Request):
         "home.html",
         {"request": request}
     )
+
+#Post - Criar um usuário
+@app.post("/cadastro")
+def cadastrar_usuario(
+    request: Request,
+    email: str = Form(...),
+    senha: str = Form(...),
+    db: Session = Depends(get_db) #dependência com a tabela
+):
+    #Procurar o email no banco de dados.
+    user_existente = db.query(Usuario).filter_by(email=email).first()
+
+    if user_existente:
+        return templates.TemplateResponse(
+            request,
+            "cadastro.html",
+            {"request": request, "erro": "Email: já cadastrado."}
+        )
+    #Criando um objeto
+    novo_usuario = Usuario(email=email, senha=senha)
+    db.add(novo_usuario)
+    db.commit()
+
+    return RedirectResponse(url="/login", status_code=303)
